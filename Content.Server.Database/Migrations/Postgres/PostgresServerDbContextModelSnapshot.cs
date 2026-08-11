8348e16fa3ac7c470595b5cdb2bd2f1af5529e6c
Valikzant <valikzant@aavikko.local>
2026-08-11 23:15:39 +0300

    migration: RemoveTTSVoice (drop ttsvoice column)

--- Content.Server.Database/Migrations/Postgres/PostgresServerDbContextModelSnapshot.cs
+++ Content.Server.Database/Migrations/Postgres/PostgresServerDbContextModelSnapshot.cs
@@ -1200,11 +1200,6 @@ namespace Content.Server.Database.Migrations.Postgres
                         .HasColumnType("text")
                         .HasColumnName("species");
 
-                    b.Property<string>("TTSVoice")
-                        .IsRequired()
-                        .HasColumnType("text")
-                        .HasColumnName("ttsvoice");
-
                     b.Property<string>("Voice")
                         .HasColumnType("text")
                         .HasColumnName("voice");
